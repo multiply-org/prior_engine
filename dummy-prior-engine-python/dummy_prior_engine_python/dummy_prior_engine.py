d723771c144ddf9aa10aac7fe61f5fe463f7a9a8
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-04-06 16:16:22 +0200

    set up dummies

--- dummy-prior-engine-python/dummy_prior_engine_python/dummy_prior_engine.py
+++ dummy-prior-engine-python/dummy_prior_engine_python/dummy_prior_engine.py
@@ -1,12 +1,12 @@
 class dummy_prior_engine:
 
     def create_prior(self, aux_data):
-        print 'I create a prior'
+        print 'Creating a prior'
         return []
 
     def save_prior(self, prior):
-        print 'I save a prior'
+        print 'Saving a prior'
 
     def get_prior(self, prior_id):
-        print 'I return a prior'
+        print 'Returning a prior'
         return []

b8f7300c82827e89cfd33d4db318514347a794a0
TonioF <tonio.fincke@brockmann-consult.de>
2017-06-25 13:55:44 +0200

    fix for python 3

--- multiply_prior_engine/dummy_prior_engine.py
+++ multiply_prior_engine/dummy_prior_engine.py
@@ -1,12 +1,12 @@
 class DummyPriorEngine:
 
     def create_prior(self, aux_data):
-        print 'Creating a prior'
+        print ('Creating a prior')
         return []
 
     def save_prior(self, prior):
-        print 'Saving a prior'
+        print ('Saving a prior')
 
     def get_prior(self, prior_id):
-        print 'Returning a prior'
+        print ('Returning a prior')
         return []

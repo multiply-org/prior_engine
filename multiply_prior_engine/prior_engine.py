04ee1f56037191ff2522e55f7b975183ae636f2f
Alex <alexander.loew@lmu.de>
2017-06-27 22:04:57 +0200

    added skeleton for roughness prior

--- multiply_prior_engine/prior_engine.py
+++ multiply_prior_engine/prior_engine.py
@@ -24,6 +24,10 @@ class PriorEngine(object):
     def _get_prior(self, p):
         if p == 'sm':
             P = SoilMoisturePrior(ptype=self.priors[p]['type'])
+        elif p == 'vegetation':
+            assert False, 'The veg prior should provide cross-correlated prior information of vegetation characteristics'
+        elif p == 'roughness':
+            P = RoughnessPrior(ptype=self.priors[p]['type'])
         else:
             assert False, 'Invalid prior'
 
@@ -31,8 +35,6 @@ class PriorEngine(object):
         P.calc()
 
         return P.file  # return filename where prior file is located
-
-
 
 
 class Prior(object):
@@ -46,23 +48,92 @@ class Prior(object):
     def calc(self):
         assert False, 'Should be implemented in child class'
 
+class MapPrior(Prior):
+    """
+    prior which is based on a LC map
+    and a LUT
+    """
+    def __init__(self, **kwargs):
+        """
+        Parameters
+        ----------
+        lut_file : str
+            filename of LUT file
+        lc_file : str
+            filename of landcover file
+        """
+        super(MapPrior, self).__init__(**kwargs)
+        self.lut_file = kwargs.get('lut_file', None)
+        assert self.lut_file is not None, 'LUT needs to be provided'
+
+        self.lc_file = kwargs.get('lc_file', None)
+        assert self.lc_file is not None, 'LC file needs to be provided'
+
+        # check that files exist
+        assert os.path.exists(self.lc_file)
+        assert os.path.exists(self.lut_file)
+
+
+
 
 class SoilMoisturePrior(Prior):
+    """
+    Soil moisture prior generation
+    """
     def __init__(self, **kwargs):
         super(SoilMoisturePrior, self).__init__(**kwargs)
 
     def calc(self):
         if self.ptype == 'climatology':
             self.file = self._get_climatology_file()
+        elif self.ptype == 'recent':
+            self.file = self._get_recent_sm_proxy()
         else:
             assert False
 
     def _get_climatology_file(self):
-
+        """
+        return filename of preprocessed climatology
+        """
         f = tempfile.mktemp()
         os.system('touch ' + f)
         return f
 
+    def _get_recent_sm_proxy(self):
+        assert False
 
+
+class RoughnessPrior(MapPrior):
+
+    def __init__(self, **kwargs):
+        super(RoughnessPrior, self).__init__(**kwargs)
+
+    def calc(self):
+        if self.ptype == 'climatology':
+            self._read_lut()
+            self._read_lc()
+            self._map_lut()
+            self.file =  self.save()
+        else:
+            assert False
+
+    def _read_lut(self):
+        self.lut = 'abc'
+
+    def _read_lc(self):
+        self.lc = 'efg'
+
+    def _map_lut(self):
+        """
+        should do the mapping of s, l, ACL type
+        """
+        self.roughness = self.lut + self.lc
+
+
+    def save(self):
+        """
+        save mapped roughness data to file
+        """
+        return tempfile.mktemp(suffix='.nc')
 
 
--- tests/test_prior.py
+++ tests/test_prior.py
@@ -2,10 +2,16 @@ import unittest
 import sys
 import os
 
+import tempfile
+
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + os.sep + '..' )
-from multiply_prior_engine import SoilMoisturePrior
+from multiply_prior_engine import SoilMoisturePrior, RoughnessPrior
 
 class TestPrior(unittest.TestCase):
+
+    def gen_file(self, s):
+        os.system('touch ' + s)
+    
     def test_sm_prior(self):
         S = SoilMoisturePrior(ptype='climatology')
 
@@ -14,4 +20,16 @@ class TestPrior(unittest.TestCase):
         S.calc()
         print S.file
         self.assertTrue(os.path.exists(S.file))
+
+
+    def test_roughness(self):
+        lut_file = tempfile.mktemp(suffix='.lut')
+        lc_file = tempfile.mktemp(suffix='.nc')
+        self.gen_file(lut_file)
+        self.gen_file(lc_file)
+        P = RoughnessPrior(ptype='climatology', lut_file=lut_file, lc_file=lc_file)
+
+        P.calc()
+
+
 

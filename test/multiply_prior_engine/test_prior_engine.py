ebc7be034f6c400b05802022c17f96107e21cfdb
Thomas Ramsauer <t.ramsauer@iggf.geo.uni-muenchen.de>
2017-11-16 20:36:53 +0100

    fixed tests. adjusted directories and names.

--- test/multiply_prior_engine/test_prior_engine.py
+++ test/multiply_prior_engine/test_prior_engine.py
@@ -4,8 +4,10 @@ import pytest
 from nose.tools import with_setup
 from multiply_prior_engine import PriorEngine, SoilMoisturePrior
 
+
 def test_priorengine_init():
-    P = PriorEngine(config='../tests/test_config_prior.yml')
+    P = PriorEngine(config='./test/multiply_prior_engine/'
+                           'test_config_prior.yml')
 
     assert P.configfile is not None
     assert type(P.configfile) is str
@@ -13,8 +15,9 @@ def test_priorengine_init():
 
 
 def test_priorengine_get_priors():
-    P = PriorEngine(config='../tests/test_config_prior.yml')
-    assert type(P.get_priors()) is dict
+    P = PriorEngine(config='./test/multiply_prior_engine/'
+                           'test_config_prior.yml')
+    assert type(P.get_priors()) is tuple
 
 
 def test_sm_prior_init():
@@ -39,18 +42,19 @@ def test_sm_prior_invalid_ptype():
 
 
 def test_calc_config():
-    P = PriorEngine(config='../tests/test_config_prior.yml')
+    P = PriorEngine(config='./test/multiply_prior_engine/'
+                           'test_config_prior.yml')
     S = SoilMoisturePrior(config=P.config,
                           ptype='climatology')
     assert type(S.config) is dict
 
 
 def test_calc_output():
-    P = PriorEngine(config='../tests/test_config_prior.yml')
+    P = PriorEngine(config='./test/multiply_prior_engine/'
+                           'test_config_prior.yml')
     S = SoilMoisturePrior(config=P.config,
                           ptype='climatology')
-    S.calc()
-    assert os.path.exists(S.file)
+    assert type(S.calc()) is tuple
 
 
 # def test_roughness():

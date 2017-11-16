b7e7c9994a0f10992824263dd4b91ea7ada9fdf6
Thomas Ramsauer <t.ramsauer@iggf.geo.uni-muenchen.de>
2017-11-16 20:35:00 +0100

    return state vector/covariance matrix.change ROI reading. update docs,credits.

--- multiply_prior_engine/prior_engine.py
+++ multiply_prior_engine/prior_engine.py
@@ -2,24 +2,32 @@
 # -*- coding: utf-8 -*-
 
 """
-    Soil Moisture Prior Engine
+    Soil Moisture Prior Engine for MULTIPLY.
 
     Copyright (C) 2017  Thomas Ramsauer
 """
 
 
-import tempfile
 import datetime
 import os
-# import xarray as xr
-import yaml
-import numpy as np
-import scipy as sp
-from scipy import spatial
-# import re
-import ast
+import tempfile
 
+import numpy as np
+import shapely
+import shapely.wkt
+import yaml
 from netCDF4 import Dataset
+from scipy import spatial
+
+
+__author__ = ["Alexander Löw", "Thomas Ramsauer"]
+__copyright__ = "Copyright 2017, Thomas Ramsauer"
+__credits__ = ["Alexander Löw", "Thomas Ramsauer"]
+__license__ = "GPLv3"
+__version__ = "0.0.1"
+__maintainer__ = "Thomas Ramsauer"
+__email__ = "t.ramsauer@iggf.geo.uni-muenchen.de"
+__status__ = "Prototype"
 
 
 class PriorEngine(object):
@@ -39,9 +47,11 @@ class PriorEngine(object):
             'There is no prior specified in configfile.'
 
     def get_priors(self):
-        """Get prior files.
+        """Get prior data.
+           calls *_get_prior* for all priors in config.
 
-        :returns: dictionary with prior names/filepath as key/value
+        :returns: dictionary with prior names/(state vector,
+                  inverse covariance matrix) as key/value
         :rtype: dictionary
 
         """
@@ -50,11 +60,16 @@ class PriorEngine(object):
         for p in self.priors.keys():
             res.update({p: self._get_prior(p)})
 
-        return res
+        # temp fix:
+        return res['sm_clim']
+
+        # TODO return concatenated priors, something like
+        # return list(zip(res.values()))
 
     def _get_config(self):
-        """Load confing from self.configfile.
-           writes to self.config.
+        """
+        Load confing from self.configfile.
+        writes to self.config.
 
         :returns: nothing
         """
@@ -65,9 +80,10 @@ class PriorEngine(object):
              .format(self.configfile))
 
     def _get_prior(self, p):
-        """Called by get_priors for all prior keys in config.
-           For specific prior (e.g. sm_clim) get prior info and calculate
-           prior.
+        """
+        Called by get_priors for all prior keys in config.
+        For specific prior (e.g. sm_clim) get prior info and calculate
+        prior.
 
         :param p: prior name (e.g. sm_clim)
         :returns: prior file
@@ -98,11 +114,9 @@ class PriorEngine(object):
             assert False, 'Invalid prior'
 
         # calculate prior
-        # prior, C_prior_inv = prior.calc()
+        # state_vector, c_prior_inv = prior.calc()
 
-        prior.calc()
-        # return filename where prior file is located
-        return prior.file
+        return prior.calc()
 
 
 class Prior(object):
@@ -119,7 +133,7 @@ class Prior(object):
         assert False, 'Should be implemented in child class'
 
     def concat_priors(self):
-        """concat individual priors and covaranc matrices
+        """concat individual priors and covariance matrices
 
         :returns:
         :rtype:
@@ -169,7 +183,8 @@ class SoilMoisturePrior(Prior):
         :returns: nothing
         """
         if self.ptype == 'climatology':
-            self.file = self._calc_climatological_prior()
+            # self.prior = self._calc_climatological_prior()
+            return self._calc_climatological_prior()
         # elif self.ptype == 'recent':
             # self.file = self._get_recent_sm_proxy()
         else:
@@ -177,7 +192,8 @@ class SoilMoisturePrior(Prior):
 
     def _get_climatology_file(self):
         """
-        Load pre-processed climatology into self.clim_data
+        Load pre-processed climatology into self.clim_data.
+        Part of prior._calc_climatological_prior().
 
         """
         assert (self.config['Prior']['priors']['sm_clim']
@@ -189,34 +205,37 @@ class SoilMoisturePrior(Prior):
         self.clim_data = Dataset(self.config['Prior']['priors']['sm_clim']
                                  ['climatology_file'])
 
-        # fd, path = tempfile.mkstemp()
-        # os.write(fd, 'tempfile')
-        # os.close(fd)
-
     def _extract_climatology(self):
         """
-        Extract climatology values for ROI
+        Extract climatology values for ROI.
+        Part of _clac_climatological_prior().
+
         """
         clim = self.clim_data.variables['sm'][:]
         std = self.clim_data.variables['sm_stdev'][:]
         lats = self.clim_data.variables['lat'][:]
         lons = self.clim_data.variables['lon'][:]
 
-        # TODO wonky ROI loading
-        ROI_ = (self.config['General'][0]['roi']
-                .split(' ', 1)[1]
-                .replace(' ', ''))
+        ROI_wkt = shapely.wkt.loads(self.config['General']['roi'])
 
-        # make string a tuple
-        ROI = ast.literal_eval(ROI_)
+        # minx, miny, maxx, maxy:
+        minx, miny, maxx, maxy = ROI_wkt.bounds
+        ROI = [(minx, miny), (maxx, maxy)]
 
         # TODO insert check for ROI bounds:
         # if POI[0] < np.amin(lats) or POI[0] > np.amax(lats) or\
         #    POI[1] < np.amin(lons) or POI[1] > np.amax(lons):
         #     raise ValueError("POI's latitude and longitude out of bounds.")
+
+        # stack all raveled lats and lons from climatology
+        # combined_LAT_LON results in e.g.
+        # [[ 34.625 -10.875]
+        #  [ 34.625 -10.625]
+        #  [ 34.625 -10.375]...]
         combined_LAT_LON = np.dstack([lats.ravel(), lons.ravel()])[0]
         mytree = spatial.cKDTree(combined_LAT_LON)
         idx, idy = [], []
+        # for all coordinate pairs in ROI search indexes in combined_LAT_LON:
         for i in range(len(ROI)):
             dist, indexes = mytree.query(ROI[i])
             x, y = tuple(combined_LAT_LON[indexes])
@@ -236,16 +255,23 @@ class SoilMoisturePrior(Prior):
         # sm_area_std = np.std(sm_area, axis=(1, 2))
         # sm_area_mean = np.mean(sm_area, axis=(1, 2))
 
+        # TODO Respect spatial resolution in config file.
+        #      Adjust result accordingly.
+
         # print(sm_area)
         self.clim = sm_area
         self.std = sm_area_std
 
     def _calc_climatological_prior(self):
         """
-        Calculate climatological prior
+        Calculate climatological prior.
+        Reads climatological file and extracts proper values for given
+        timespan and -interval.
+        Then converts the means and stds to state vector and covariance
+        matrices.
 
-        :returns: filepath to temp prior file
-        :rtype: str
+        :returns: state vector and covariance matrix
+        :rtype: tuple
 
         """
         self._get_climatology_file()
@@ -254,21 +280,23 @@ class SoilMoisturePrior(Prior):
         # TODO limit to months
 
         # date_format = ('%Y-%m-%d')
-        s = self.config['General'][1]['start_time']
-        e = self.config['General'][2]['end_time']
+        s = self.config['General']['start_time']
+        e = self.config['General']['end_time']
         t_span = (e-s).days + 1
         # print(t_span)
 
+        # create list of month ids for every queried point in time:
         idt = [(s+datetime.timedelta(x)).month
                for x in range(t_span)]
         # idt_unique = list(set(idt))
 
-        # create nd array with correct dimensions
+        # create nd array with correct dimensions (time, x, y):
         p = np.ndarray(shape=(len(idt), self.clim.shape[1],
                               self.clim.shape[2]),
                        dtype=float)
         std = p.copy()
 
+        # read correspending data into mean and std arrays:
         for i in range(len(idt)):
             p[i, :, :] = self.clim[idt[i]-1, :, :]
             std[i, :, :] = self.std[idt[i]-1, :, :]
@@ -278,30 +306,19 @@ class SoilMoisturePrior(Prior):
         sm_unc = (std/np.mean(self.clim))
         # inverse covariance matrix
         diagon = (1./sm_unc)
+        # print(diagon.shape)
 
-        def create_sparse_matrix(a):
-            return sp.sparse.lil_matrix(np.eye(t_span)*a)
+        # def create_sparse_matrix(a):
+        #     return sp.sparse.lil_matrix(np.eye(t_span)*a)
 
-        C_prior_inv = np.apply_along_axis(create_sparse_matrix, 0, diagon)
-
-        # print(C_prior_inv.shape, C_prior_inv)
+        # C_prior_inv = np.apply_along_axis(create_sparse_matrix, 0, diagon)
+        C_prior_inv = diagon
 
         # DISCUSS TODO
         # rather write to self.'prior_key' to easy concatenate afterwards
         # via concat_priors.
 
-        # return p, C_prior_inv
-
-        # ------------------------
-        # Create temp prior for now
-        prior = np.array('')
-        fd, path = tempfile.mkstemp()
-        os.write(fd, prior)
-        os.close(fd)
-
-        # return prior.file
-        # self.file = path
-        return path
+        return p, C_prior_inv
 
     def _get_recent_sm_proxy(self):
         assert False, "recent sm proxy not implemented"
@@ -340,8 +357,16 @@ class RoughnessPrior(MapPrior):
         return tempfile.mktemp(suffix='.nc')
 
 
+def get_mean_state_vector(config_file):
+    """return state vector and inverse covariance matrix for priors.
+
+    :param config_file: path to config file
+    :returns: state vector and inverse covariance matrix
+    :rtype: dictionary
+
+    """
+    return PriorEngine(config="./sample_config_prior.yml").get_priors()
+
+
 if __name__ == '__main__':
-    # P = PriorEngine(config="./sample_config_prior.yml")
-    # priors = P.get_priors()
-    # print(priors)
-    pass
+    get_mean_state_vector(config_file="./sample_config_prior.yml")

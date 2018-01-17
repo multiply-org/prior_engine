3e637553ffab772785aed8e7240c64eaca896497
Thomas Ramsauer <t.ramsauer@iggf.geo.uni-muenchen.de>
2018-01-17 14:13:34 +0100

    update installation routines: setup.py and travis.yml

--- setup.py
+++ setup.py
@@ -2,9 +2,36 @@
 
 from setuptools import setup
 
+requirements = [
+    'nose',
+    'shapely',
+    'pyyaml',
+    'pytest', 'nose',
+    'shapely',
+    'pyyaml',
+    'datetime',
+    'os',
+    're',
+    'numpy',
+    'shapely',
+    'netCDF4',
+    'scipy',
+    'yaml',
+    'dateutil'
+]
+
 setup(name='multiply-prior-engine',
       version='0.1',
       description='MULTIPLY Prior Engine',
       author='MULTIPLY Team',
       packages=['multiply_prior_engine'],
-     )
+      entry_points={
+          'file_system_plugins': [
+              'local_file_system = multiply_data_access:local_file_system.LocalFileSystemAccessor',
+          ],
+          'meta_info_provider_plugins': [
+              'json_meta_info_provider = multiply_data_access:json_meta_info_provider.JsonMetaInfoProviderAccessor',
+          ],
+      },
+      install_requires=requirements
+)

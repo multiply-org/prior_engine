ccc19c7e3b7780927d4e7de6372379af601daa6b
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-07-05 12:05:05 +0200

    included import

--- multiply_prior_engine/__init__.py
+++ multiply_prior_engine/__init__.py
@@ -1,5 +1,5 @@
 from .prior_engine import PriorEngine
 # from .prior_engine import *
-# from .prior import *
+from .prior import Prior
 # from .soilmoisture_prior import *
 # from .vegetation_prior import *

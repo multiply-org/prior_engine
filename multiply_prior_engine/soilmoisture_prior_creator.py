4558b3ff5dcdc93a75f26c79d5977fbcf275510c
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2018-10-19 14:34:00 +0200

    removed obsolete todo

--- multiply_prior_engine/soilmoisture_prior_creator.py
+++ multiply_prior_engine/soilmoisture_prior_creator.py
@@ -321,7 +321,6 @@ class SoilMoisturePriorCreator(PriorCreator):
             out_fn = os.path.join(self.output_directory, temp_fn)
             vrt_options = gdal.BuildVRTOptions(outputBounds=(-180, -90, 180, 90))
             gdal.BuildVRT(out_fn, fn, options=vrt_options)
-            # TODO VRT is not necessary global.
             res = '{}'.format(out_fn)
             assert os.path.isfile(res), "{} is not a file.".format(res)
             self._check_gdal_compliance(res)
